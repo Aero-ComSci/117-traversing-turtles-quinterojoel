698b4d59084338c38106df3c2bb10c6aca6d1a13
quinterojoel <157430955+quinterojoel@users.noreply.github.com>
2024-10-14 13:32:43 -0700

    Update spiral.py

--- spiral.py
+++ spiral.py
@@ -1,39 +1,42 @@
 #   a117_traversing_turtles.py
-#   Add code to make turtles move in a circle and change colors.
+
 import turtle as trtl
 
-# create an empty list of turtles
 my_turtles = []
 
-# use interesting shapes and colors
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
-turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic", "arrow", "turtle", "circle", "square", "triangle", "classic", "arrow", "turtle", "circle", "square", "triangle", "classic", "arrow", "turtle", "circle", "square", "triangle", "classic"]
+turtle_colors = ["red", "blue", "green", "orange", "purple", "gold", "red", "blue", "green", "orange", "purple", "gold", "red", "blue", "green", "orange", "purple", "gold", "red", "blue", "green", "orange", "purple", "gold"]
 
 for s in turtle_shapes:
-  t = trtl.Turtle(shape=s)
-  my_turtles.append(t)
+    t = trtl.Turtle(shape=s)
+    my_turtles.append(t)
+    t.speed(0)
 
-#makes all turtles start at origin
 startx = 0
 starty = 0
 lastangle = 0
+spiral = 30
+spiralthing = 0
 
-#makes all turtles move to the current coords, one by one
 for t in my_turtles:
-  new_color = turtle_colors.pop()
-  t.fillcolor(new_color)
-  t.color(new_color)
-  t.penup()
-  t.goto(startx, starty)
-  t.setheading(lastangle)
-  t.pendown()
-  t.forward(50)
-  startx = t.xcor()
-  starty = t.ycor()
-  lastangle -= 45
-
-
-#change coords every time a turtle moves, so they don't all move to the same place
+    if spiralthing == 6:
+        spiral -= 5
+    if spiralthing == 12:
+        spiral -= 5
+    if spiralthing == 18:
+        spiral -= 5
+    new_color = turtle_colors.pop()
+    t.fillcolor(new_color)
+    t.color(new_color)
+    t.penup()
+    t.goto(startx, starty)
+    t.setheading(lastangle)
+    t.pendown()
+    t.forward(50)
+    startx = t.xcor()
+    starty = t.ycor()
+    lastangle -= spiral
+    spiralthing += 1
 
 wn = trtl.Screen()
 wn.mainloop()
